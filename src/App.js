f869fd696124c105e94ba50b2cc0cb1b6c6bee10
jameson.trinker <jameson.trinker@cellularsales.com>
2019-12-03 21:24:12 -0500

    wip option alpha challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import { Button, Card, Row, Col, Checkbox } from 'react-materialize';
 import './App.css';
+import { buildEndpoint } from './helpers.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Dashboard extends React.Component {
+  state = {
+    stockData: [],
+    symbols: []
+  }
+
+  setTicker = (event) => {
+    console.log(event.target.value);
+    // copy symbols array and push event.target.value
+    // this.setState({ symbols: nextSymbols });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <StockPicker bubbleTicker={this.setTicker} />
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+class StockPicker extends React.Component {
+  addTicker = (event) => {
+    this.props.bubbleTicker(event)
+  } 
+
+  render () {
+    return (
+      <div className="col s12">
+        <Checkbox label="spy" value="spy" className="ticker-check" onChange={this.addTicker} checked={this.isChecked} />
+        <Checkbox label="vix" value="vix" className="ticker-check" onChange={this.addTicker} checked={this.isChecked} />
+        <Checkbox label="tlt" value="tlt" className="ticker-check" onChange={this.addTicker} checked={this.isChecked} />
+
+        <Button large>Get Ticker Data</Button>
+      </div>
+    )
+  }
+}
+
+export default Dashboard;
---
+++ src/helpers.js
@@ -0,0 +1,18 @@
+// API INFO
+const ROOT = 'https://api.worldtradingdata.com/api/v1/stock?symbol=';
+const TOKEN = '=z5IhiYKbAvjvwC05hl1759smWFH5tHMw5HobVE72IRV5d0hlg3863Ygzym5G';
+
+export function buildEndpoint(tickerArray) {
+    let symbolString = '';
+    tickerArray.forEach((value, index) => {
+        if (index === tickerArray.length - 1) {
+            symbolString = `${symbolString}${tickerArray[index]}`;
+        } else {
+            symbolString = `${symbolString}${tickerArray[index]},`;
+        }
+    });
+    
+    let endPoint = ROOT + symbolString + '?api_token' + TOKEN;
+    return endPoint;
+};
+  
