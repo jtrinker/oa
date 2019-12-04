0f1259972075621ee0fe1fdc14c5952a48c4c7e6
jameson.trinker <jameson.trinker@cellularsales.com>
2019-12-04 00:49:59 -0500

    Updates

---
+++ precache-manifest.f2ed5df252f750221606a9418df54fec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eddfcd9e04c463dcac8b9bca2b02936f",
+    "url": "/jtrinker/oa/index.html"
+  },
+  {
+    "revision": "68bf23b6bb7c7834adb1",
+    "url": "/jtrinker/oa/static/css/main.6e3aae00.chunk.css"
+  },
+  {
+    "revision": "653ba1084f9fc35d950d",
+    "url": "/jtrinker/oa/static/js/2.99444517.chunk.js"
+  },
+  {
+    "revision": "68bf23b6bb7c7834adb1",
+    "url": "/jtrinker/oa/static/js/main.6cf1966e.chunk.js"
+  },
+  {
+    "revision": "528a54f06aad91864df3",
+    "url": "/jtrinker/oa/static/js/runtime-main.921b3f43.js"
+  }
+]);
